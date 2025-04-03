f3d9cad75cb0cac845d0212a84b180c37103629f
Arib Farooqui <aribbabar100@gmail.com>
2025-04-02 23:57:52 -0400

    add static adapter for Svelte and remove redirects configuration

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,24 @@
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import adapter from "@sveltejs/adapter-static";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 
 export default {
   // Consult https://svelte.dev/docs#compile-time-svelte-preprocess
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-}
+  kit: {
+    adapter: adapter({
+      // See below for an explanation of these options
+      config: undefined,
+      platformProxy: {
+        configPath: undefined,
+        environment: undefined,
+        persist: undefined
+      },
+      fallback: "plaintext",
+      routes: {
+        include: ["/*"],
+        exclude: ["<all>"]
+      }
+    })
+  }
+};
